07c01a1ea4df07c7a612e91972a528d0c0b54b7a
Olamilekan <olamilekanomosanya@gmail.com>
2022-10-12 18:08:58 +0100

    run migration

--- resources/views/admin/posts/add.blade.php
+++ resources/views/admin/posts/add.blade.php
@@ -14,10 +14,10 @@
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
     <!-- Icons -->
-    <link rel="stylesheet" href="{{ asset('assets/backend/vendor/nucleo/css/nucleo.css" type="text/css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/backend/vendor/nucleo/css/nucleo.css') }}" type="text/css">
     <link rel="stylesheet" href="{{ asset('assets/backend/vendor/@fortawesome/fontawesome-free/css/all.min.css') }}" type="text/css">
     <!-- Argon CSS -->
-    <link rel="stylesheet" href="{{ asset('assets/backend/css/argon.css?v=1.2.0" type="text/css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/backend/css/argon.css?v=1.2.0') }}" type="text/css">
     <link rel="stylesheet" href="{{ asset('assets/plugins/froala/css/froala_editor.pkgd.min.css') }}" type="text/css">
     <style>
         #fr-logo {
